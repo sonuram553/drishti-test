1bc880d405f0fa1b80e32bee8f71b315a0f3d247
Sonu Ram <sonuram6960@gmail.com>
2022-11-25 21:14:46 +0530

    Delete auto generated files and create ActionCard component

---
+++ src/ActionCard.jsx
@@ -0,0 +1,17 @@
+const ActionCard = () => {
+  return (
+    <article className="action-card">
+      <div className="action-card__thumbnail"></div>
+      <h2>Bake</h2>
+      <div>
+        <p>LASAGNA</p> <p>OVEN1</p>
+      </div>
+      <div>
+        <p>duration</p>
+        <p>start time</p>
+      </div>
+    </article>
+  );
+};
+
+export default ActionCard;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import ActionCard from "./ActionCard";
+
+const data = [
+  {
+    dish: "LASAGNA",
+    station: "OVEN1",
+    duration: 1668.488,
+    startTime: "2022-11-25T15:15:18.642Z",
+    action: "Bake",
+  },
+  {
+    dish: "LASAGNA",
+    station: "OVEN1",
+    duration: 1668.488,
+    startTime: "2022-11-25T15:15:18.642Z",
+    action: "Bake",
+  },
+  {
+    dish: "LASAGNA",
+    station: "OVEN1",
+    duration: 1668.488,
+    startTime: "2022-11-25T15:15:18.642Z",
+    action: "Bake",
+  },
+  {
+    dish: "LASAGNA",
+    station: "OVEN1",
+    duration: 1668.488,
+    startTime: "2022-11-25T15:15:18.642Z",
+    action: "Bake",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ActionCard />
     </div>
   );
 }
