c7e637125eedcb80a4679238733e065a88e13d62
Sonu Ram <sonuram6960@gmail.com>
2022-11-26 01:07:59 +0530

    Update total count as well on filter change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,22 +17,6 @@ function App() {
     station: "any",
   });
 
-  useEffect(() => {
-    // Reset pagination on filter change
-    onPageChange("reset");
-  }, [filtersApplied]);
-
-  const onPageChange = (action) => {
-    if (action === "reset") setPage((page) => ({ ...page, skip: 0 }));
-    else if (action === "next")
-      setPage((page) => ({ ...page, skip: page.skip + page.limit }));
-    else setPage((page) => ({ ...page, skip: page.skip - page.limit }));
-  };
-
-  const onUpdateFilter = (type, selected) => {
-    setFiltersApplied((filter) => ({ ...filter, [type]: selected }));
-  };
-
   // useMemo is used because we don't want to reply filtering logic on page change.
   const filteredData = useMemo(
     () =>
@@ -57,28 +41,54 @@ function App() {
     [filtersApplied]
   );
 
+  useEffect(() => {
+    // Reset pagination on filter change
+    resetPagination();
+  }, [filtersApplied]);
+
+  const onPageChange = (action) => {
+    if (action === "next")
+      setPage((page) => ({ ...page, skip: page.skip + page.limit }));
+    else setPage((page) => ({ ...page, skip: page.skip - page.limit }));
+  };
+
+  const resetPagination = () => {
+    setPage((page) => ({ ...page, skip: 0, total: filteredData.length }));
+  };
+
+  const onUpdateFilter = (type, selected) => {
+    setFiltersApplied((filter) => ({ ...filter, [type]: selected }));
+  };
+
   const pagedData = () => {
     const { skip, limit } = page;
     return filteredData.slice(skip, skip + limit);
   };
 
   return (
-    <section>
-      <FilterList
-        onChange={onUpdateFilter}
-        items={[
-          { type: "action", values: actions, selected: filtersApplied.action },
-          { type: "dish", values: dishes, selected: filtersApplied.dish },
-          {
-            type: "station",
-            values: stations,
-            selected: filtersApplied.station,
-          },
-        ]}
-      />
-      <ActionList items={pagedData()} />
-      <Pagination {...page} onPageChange={onPageChange} />
-    </section>
+    <main>
+      <section></section>
+      <section>
+        <FilterList
+          onChange={onUpdateFilter}
+          items={[
+            {
+              type: "action",
+              values: actions,
+              selected: filtersApplied.action,
+            },
+            { type: "dish", values: dishes, selected: filtersApplied.dish },
+            {
+              type: "station",
+              values: stations,
+              selected: filtersApplied.station,
+            },
+          ]}
+        />
+        <ActionList items={pagedData()} />
+        <Pagination {...page} onPageChange={onPageChange} />
+      </section>
+    </main>
   );
 }
 
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,20 +1,32 @@
 const Pagination = ({ skip, limit, total, onPageChange }) => {
+  const isPreviousDisabled = skip === 0;
+  const isNextDisabled = skip + limit >= total;
+
   return (
-    <div>
+    <div className="pagination">
+      {/* Previous Button */}
       <button
-        onClick={() => {
-          if (skip > 0) onPageChange("previous");
-        }}
+        className={
+          "pagination__btn" +
+          (isPreviousDisabled ? " pagination__btn--disabled" : "")
+        }
+        onClick={() => !isPreviousDisabled && onPageChange("previous")}
       >
         Previous
       </button>
 
-      <p>1 to 10 of 100</p>
+      {/* Current Page */}
+      <p>{`${skip + 1} to ${
+        skip + limit < total ? skip + limit : total
+      } of ${total}`}</p>
 
+      {/* Next Button */}
       <button
-        onClick={() => {
-          if (skip + limit < total) onPageChange("next");
-        }}
+        className={
+          "pagination__btn" +
+          (isNextDisabled ? " pagination__btn--disabled" : "")
+        }
+        onClick={() => !isNextDisabled && onPageChange("next")}
       >
         Next
       </button>
